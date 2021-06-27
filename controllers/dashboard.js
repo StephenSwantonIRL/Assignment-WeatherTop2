a7ce89bb3173f48ee97cc3de91fdd03897b2cba7
Stephen <stephenswanton@gmail.com>
2021-06-27 17:47:56 +0100

    Initial Commit  - converting templates to Handlebars

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -6,7 +6,23 @@ const dashboard = {
   index(request, response) {
     logger.info("dashboard rendering");
     const viewData = {
-      title: "Template 1 Dashboard",
+      title: "WeatherTop V2 Dashboard",
+      station: [
+
+        {
+          readings: {
+            reading: [{
+              value: "name1"
+            }, {
+              value: "name2"
+            }, {
+              value: "name3"
+            }]
+          },
+          name: "Dunmore"
+        }
+      ],
+      stationempty: 0
     };
     response.render("dashboard", viewData);
   },
